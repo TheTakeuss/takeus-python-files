a89702791c21fb7533cfaa59f392ea99ec705d11
Alpha01/ <68923554+TheTakeuss@users.noreply.github.com>
2020-12-04 13:41:11 +0100

    Update crypteur.py

--- crypteur.py
+++ crypteur.py
@@ -9,6 +9,7 @@ encoded_message = ""
 temp = ""
 temp2 = ""
 temp3 = 0
+v2 = ""
 
 # DICTIONNAIRE
 data_encoded = {
@@ -163,12 +164,13 @@ data_decoded = {
 "§": ","}
 
 code = ["(", "[", "{", "$", "|", "-", "_",  "#", "=", "&", "/", "}", "]", ")"]
+codel = ["A", "g", "K", "P", "d", "P", "Z",  "r", "o", "h", "s", "l", "g", "v"]
 
 
 #Programme
 os.system('cls')
 print("----------\n|Encrypter|\n----------\n")
-print("1 - Encode\n2 - Decode\n3 - Lists\n4 - quit\n\n")
+print("1 - Encode\n2 - Decode\n3 - Lists\n4 - MoreSecureMode (IN DEV)\n5 - quit\n\n")
 temp = input("> ")
 if temp == "1":
     os.system('cls')
@@ -250,6 +252,96 @@ elif temp == "3":
         if temp != "y":
             break
 
+elif temp == "4":
+    print("----------\n|Encrypter|\n----------\n1 - Encode\n2 - Decode\n3 - Quit")
+    temp = input("> ")
+    if temp == "1":
+        os.system('cls')
+        print("----------\n|Encrypter|\n----------\n")
+        print("More Secure Mode\nthe message may not contain any special characters.\n")
+        message = input("message: ")
+        if len(message) >= 10:
+            temp3 = 1
+        elif len(message) >= 6:
+            temp3 = 0.5
+        else:
+            temp3 = 0.25
+        os.system('cls')
+        print("generating.")
+        time.sleep(temp3)
+        os.system('cls')
+        try: 
+            message = ' '.join(format(ord(x), 'b') for x in message)
+            for lettre in message:
+                temp = data_encoded.get(lettre)
+                temp3 = randint(0,1)
+                if temp3 == 1:
+                    encoded_message = encoded_message + codel[randint(1,len(codel) - 1)]
+                    encoded_message = encoded_message + temp
+                else:
+                    encoded_message = encoded_message + code[randint(1,len(code) - 1)]
+                    encoded_message = encoded_message + temp
+            print("generating..")
+            time.sleep(temp3)
+            os.system('cls')
+            print("generating...")
+            os.system('cls')
+            time.sleep(temp3)
+            os.system('cls')
+            print("----------\n|Encrypter|\n----------\n")
+            print("result:", encoded_message)
+            input("\n> (quit)")
+        except:
+            print("\nError, Invalid character(s) detected...")
+            input("\n> (quit)")
+
+
+
+    elif temp == "2":
+        os.system('cls')
+        print("----------\n|Encrypter|\n----------\n")
+        print("More Secure Mode\n")
+        encoded_message = input("Encoded Message: ")
+        if len(encoded_message) >= 15:
+            temp3 = 1
+        elif len(encoded_message) >= 10:
+            temp3 = 0.5
+        else:
+            temp3 = 0.25
+        os.system('cls')
+        print("decoding.")
+        time.sleep(temp3)
+        os.system('cls')
+        try:
+            for lettre in encoded_message:
+                temp = data_decoded.get(lettre)
+                if lettre not in code or lettre not in codel:
+                    message = message + temp
+            v2 = ''.join(chr(int(message[i*8:i*8+8],2)) for i in range(len(message)//8))
+            print("decoding..")
+            time.sleep(temp3)
+            os.system('cls')
+            print("decoding...")
+            os.system('cls')
+            time.sleep(temp3)
+            os.system('cls')
+            print("----------\n|Encrypter|\n----------\n")
+            print("\nmessage:", message)
+            input("\n> (quit)")          
+        except:
+            print("\nError, Unable to decrypt the message...")
+            input("\n> (quit)")
+
+
+
+
+
+
+        
+    else:
+        quit
+    
 else:
     quit
+
 
